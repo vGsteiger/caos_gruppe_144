750439ada538ab172d547ef7a5facc3b385fd20a
Viktor Urban <v.gsteiger@gmail.com>
2019-11-26 15:37:19 +0100

    Major code changes
    Changed to a different style to try out something new.

--- src/144RGBcode/144RGBcode.ino
+++ src/144RGBcode/144RGBcode.ino
@@ -4,10 +4,13 @@
  * by Viktor Gsteiger, Moritz Würth, Joey Zgraggen
  * 
  * Latest release:
- * 24.11.2019
+ * 26.11.2019
  * 
  * V0.1:
  * Basic set up and testing with the RGB Leds
+ * 
+ * V0.1.1:
+ * Major revamp of setup and other functions to simplify the code and to make it work.
  * 
  * TODOs for V0.2:
  * Basic effects (wave, Letters)
@@ -19,187 +22,78 @@
  * 
  */
 
-/*
- * Pin definition:
- * Pins correspont to a Arduino Mega 2560 ordered on Banggood.com
- */
-#include <SPI.h>
-#define latch_pin 44  // To be tested
-#define blank_pin 45  // To be tested (probably needs 1k ohm resistor)
-#define data_pin 51   // To be tested
-#define clock_pin 52  // To be tested
-#define cathode_pin 1 // To select either layer of our setup for the bit angle modulation
+#include <SPI.h>// SPI Library used to clock data out to the shift registers
 
-/*
- * Important variables
- */
-byte leds0[27], leds1[27];  // Storing the display information with the current colors and which leds are set on
+// push the to storage register, Pin 12 at IC
+#define latch_pin 49
+// to shut enable/disable the register. Low enables, Pin 13 at IC
+#define blank_pin 48
+// used by SPI, must be pin 51 at Mega 2560, Pin 14 at IC
+#define data_pin 51
+// used by SPI, must be 52 at mega 2560, Pin 11 at IC
+#define clock_pin 52
 
-int layer=0;                // keeps track of our current cathode layer
-int BAM_Bit, BAM_Counter=0; // Bit Angle Modulation variables to keep track of the clock 
+byte anodes[27]; // Testarray, to be replaced
 
-/*
- * SETUP BEGIN:
- */
+  void setup()
+  {
+   Serial.begin(115200); // Serial monitor for debugging 
+   SPI.begin();
+   SPI.setClockDivider(SPI_CLOCK_DIV2);//Run the data in at 16MHz/2 - 8MHz
+   
+   pinMode(latch_pin, OUTPUT);//Latch
+   pinMode(blank_pin, OUTPUT);//Output Enable  important to do this last, so LEDs do not flash on boot up
+   
+   digitalWrite(blank_pin, HIGH);//shut down the leds
+   digitalWrite(latch_pin, LOW);//shut down the leds
 
-  void setup() {
-    SPI.setBitOrder(MSBFIRST);            //Most Significant Bit First
-    SPI.setDataMode(SPI_MODE0);           // Mode 0 Rising edge of data, keep clock low
-    SPI.setClockDivider(SPI_CLOCK_DIV2);  //Run the data at 16MHz/2 - 8MHz
-    noInterrupts();                       // kill interrupts for the most important setups
-  
-    Serial.begin(115200); // Serial monitor for debugging 
-    TCCR1A = B00000000;   // Register A all 0's since we're not toggling any pins
-    TCCR1B = B00001011;   // bit 3 set to place in CTC mode, will call an interrupt on a counter match
-    TIMSK1 = B00000010;   // bit 1 set to call the interrupt on an OCR1A match
-    OCR1A=30;             // To be tested
-    
-    pinMode(latch_pin, OUTPUT); // Latch pin
-    pinMode(data_pin, OUTPUT);  // MOSI DATA pin
-    pinMode(clock_pin, OUTPUT); // SPI Clock pin
-    //BlankMode(blank_pin, OUTPUT); // Output Enable
-    SPI.begin();                // Start the SPI library
-    interrupts();               // Enable interrupts
-    
+  // Currently in testmode
   }
-
-/*
- * SETUP END
- */
-
-/*
- * LOOP BEGIN:
- */
-
-  void loop() {
-    // TODO: Interrupts with the button and sensory data. Also more functions!
+  
+  void loop()
+  {
     test();
   }
 
-/*
- * LOOP END
- */
-
-/*
- * LED BEGIN:
- */
-
-  void LED(int l, int x, int y, int red, int green, int blue){
-    
-    l = constrain (l,  0, 1);         // layer can only be 0 or 1 as we only have two layers
+  void setLedOn(int x, int y, int red, int green, int blue, int layer)
+  {
     x = constrain (x,    0, 15);      // x can only be between 0 and 15 as we have 16 leds length
     y = constrain (y, 0, 5);          // y can only be between 0 and 5 as we have 6 height
     red = constrain (red,    0, 1);   // Red can either be 1 or 0
     green = constrain (green,  0, 1); // Green can either be 1 or 0
     blue = constrain (blue,   0, 1);  // Blue can either be 1 or 0
-      
-    int whichbyte = int((x+36*y)/8);  // Calculate which byte be have to chang
-    int wholebyte = (x+36*y);           // Calculate exactly which LED we want to address (color comes later, this number will always be on the BLANK pin)
-    Serial.print(whichbyte);
-
-    switch(l) { // Decide on which layer we will have to operate on
-      case 0:   // Operate on layer 0 
-        bitWrite(leds0[whichbyte], (256-(wholebyte-(8*whichbyte))), red);     // Set the red bit of our current byte to either 0 or 1
-        bitWrite(leds0[whichbyte], (256-(wholebyte-(8*whichbyte)+1)), green); // Set the green bit of our current byte to either 0 or 1
-        bitWrite(leds0[whichbyte], (256-(wholebyte-(8*whichbyte)+2)), blue);  // Set the blue bit of our current byte to either 0 or 1
-      case 1:   // Operate on layer 1
-        bitWrite(leds0[whichbyte], wholebyte-(8*whichbyte), red);     // Set the red bit of our current byte to either 0 or 1
-        bitWrite(leds0[whichbyte], wholebyte-(8*whichbyte)+1, green); // Set the green bit of our current byte to either 0 or 1
-        bitWrite(leds0[whichbyte], wholebyte-(8*whichbyte)+2, blue);  // Set the blue bit of our current byte to either 0 or 1
-    }
+    layer = constrain (layer,  0, 1);     // layer can only be 0 or 1 as we only have two layers
     
+    int whichByte = int((x+36*y)/8);       // Calculate which byte be have to chang
+    int whichBit = (x+36*y)-(8*whichByte); // Calculate exactly which LED we want to address (color comes later, this number will always be on the BLANK pin)
+    bitWrite(anodes[whichByte], whichBit, red);
+    bitWrite(anodes[whichByte], whichBit+1, green);
+    bitWrite(anodes[whichByte], whichBit+2, blue);
+    Serial.println("Current anodes array after blue:");
+      for (int b = 7; b >= 0; b--)
+  {
+    Serial.print(bitRead(anodes[whichByte], b));
+  }    
+    shiftToShifter();
   }
 
-/*
- * LED END
- */
-
-/*
- * ISR START:
- */
-
-  ISR(TIMER1_COMPA_vect){
-
-  int shift_out; // Iterative variable used in this method
-  
-  PORTL |= 1<<4;  // Turn all LEDS off
-
-  // Each layer has the chance to light up twice per 4 bit angle modulation
-  if(BAM_Counter==8) {
-    BAM_Bit++;
-  } else {
-    if(BAM_Counter==24) {
-      BAM_Bit++;
-    } else {
-      if(BAM_Counter==56) {
-        BAM_Bit++;
-      }
-    }
-  }
-  
-  BAM_Counter++;
-  
-  switch (BAM_Bit){ // Depending on the BAM bit we will now light up either layer 0 or 1. We change layer with the 
-  case 0:           // method changeLayer()
-   changeLayer(0);
-   for(shift_out=0; shift_out<27; shift_out++) {
-    SPI.transfer(leds0[shift_out]);
+  void shiftToShifter() 
+  {
+    digitalWrite(blank_pin, HIGH);//shut down the leds
+    for(int i = 0; i < 27; i++) {
+    SPI.transfer(anodes[0]);
    }
-   break;
-  case 1:
-   changeLayer(1);
-   for(shift_out=0; shift_out<27; shift_out++) {
-    SPI.transfer(leds1[shift_out]);
-   }
-   break;
-  case 2:
-   changeLayer(0);
-   for(shift_out=0; shift_out<27; shift_out++) {
-    SPI.transfer(leds0[shift_out]);
-   }
-   break;
-  case 3:
-   changeLayer(1);
-   for(shift_out=0; shift_out<27; shift_out++) {
-    SPI.transfer(leds1[shift_out]);
-   }
-   // If we have reached the end of one bit angle modulation counter we reset he counter and the BAM_bit:
-   if(BAM_Counter==120){
-    BAM_Counter=0;
-    BAM_Bit=0;
-   }
-   break;
-  }
-    
-  PORTL |= 1<<5;    //Latch pin HIGH
-  PORTL &= ~(1<<5); //Latch pin LOW
-  PORTL &= ~(1<<4); //Blank pin LOW to turn on the LEDs with the new data
-
-  pinMode(blank_pin, OUTPUT);//moved down here so outputs are all off until the first call of this function
+   
+     // shift register to storage register
+    digitalWrite(latch_pin, HIGH);
+    digitalWrite(latch_pin, LOW);
+    digitalWrite(blank_pin, LOW);  //enable pins
+    delay(50);
   }
 
-/*
- * ISR END
- */
-
-  void changeLayer(int l){
-    l = constrain (l,  0, 1); // Layer can only be 1 or 0
-    if(l == 0) {
-      digitalWrite(cathode_pin,LOW);  // Change the cathode layer with the cathope_pin to 0
-    } else {
-      digitalWrite(cathode_pin,HIGH); // Change the cathode layer with the cathope_pin to 1
-    }
-  }
-
-/*
- * ANIMATIONS START:
- */
-
-  void test(){  // Test function for debugging:
-    //for (int i=0;i<12;i++){
-    //  for (int j=0;i<6;i++){
-        delay(1000);
-        LED (0,2,0, 0, 0, 1);
-      //}
-    //}
+  void test()
+  {
+    anodes[0] = B11111110;
+    shiftToShifter();
+    //setLedOn(0,0,1,0,0,0);
   }
