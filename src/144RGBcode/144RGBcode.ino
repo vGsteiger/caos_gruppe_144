ebb95e6a0f2d04a66e5c624eae3bdff9555e4cf9
Viktor Urban <v.gsteiger@gmail.com>
2019-12-02 11:25:17 +0100

    Fixed some errors
    Had a stupid error in the whichByte formula.

--- src/144RGBcode/144RGBcode.ino
+++ src/144RGBcode/144RGBcode.ino
@@ -82,14 +82,14 @@ int dispArray[6][12];
     blue = constrain (blue,   0, 1);  // Blue can either be 1 or 0
     layer = constrain (layer,  0, 1);     // layer can only be 0 or 1 as we only have two layers
     
-    int whichByte = int(((x+36*y)+1)/8);       // Calculate which byte be have to change
-    int whichBit = ((y*36+x*3)+1)%8;          //
+    int whichByte = int(((x*3+36*y)+1)/8);       // Calculate which byte be have to change
+    int whichBit = ((y*36+x*3)+1)%8;          //  Calculate which bit in that byte we have to set on
 
-    Serial.println("Currently in byte:");
-    Serial.println(whichByte);
+    //Serial.println("Currently in byte:");
+    //Serial.println(whichByte);
 
-    Serial.println("Currently in bit:");
-    Serial.println(whichBit);
+    //Serial.println("Currently in bit:");
+    //Serial.println(whichBit);
     /**
      * Choses between the two available layers of our LED RGB Cubic.
      */
@@ -182,11 +182,11 @@ int dispArray[6][12];
   void test()
   {
     //for(int i = 0; i < 5; i++) {
-      setLedOn(4,0,1,0,0,0);
+      setLedOn(3,0,1,0,0,0);
       shiftToShifter(100);
-      setLedOn(4,0,0,1,0,0);
+      setLedOn(3,0,0,1,0,0);
       shiftToShifter(100);
-      setLedOn(4,0,0,0,1,1);
+      setLedOn(3,0,0,0,1,0);
       shiftToShifter(100);
     //}
   }
@@ -248,7 +248,7 @@ int dispArray[6][12];
         }
       }
     }
-    //dispArray = tempArray;
+    memcpy(dispArray, tempArray, sizeof(tempArray));
   }
 
   /**
