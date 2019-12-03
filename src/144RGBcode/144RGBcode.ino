752cb50df40a4c2f3d25f01ac2fa0286c95acde6
Viktor Urban <v.gsteiger@gmail.com>
2019-12-03 14:38:45 +0100

    Fixed master after buggy merge

--- src/144RGBcode/144RGBcode.ino
+++ src/144RGBcode/144RGBcode.ino
@@ -265,22 +265,9 @@ int dispArray[6][12];
         }
       }
     }
-    dispArray = tempArray;
+    memcpy(dispArray, tempArray, sizeof(tempArray));
   }
 
-  /**
-   * Sets the the 2d Array given to a specific height to a single color.
-   * Combinations of r, g ,b.
-   */
-  void setLed2DArraySingleColor(int currArray[][12], int layer, int r, int g, int b, int maxH, int maxW) {
-    for(int x=0;x<maxW;x++) {
-      for(int y=0;y<maxH;y++) {
-        if(currArray[y][x] == 1) {
-          setLedOn(x,y,r,g,b,layer);
-        }
-      }
-    }
-  }
   /**
    * To handle the interrupt of the button input.
    */
