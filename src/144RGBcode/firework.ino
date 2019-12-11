b2b8a909a09683d39b1fb931d813ab26786947c3
Viktor Urban <v.gsteiger@gmail.com>
2019-12-11 09:26:31 +0100

    Further implementation of Firework
    Implemented the rocket and colour struct to save a rocket, spawn a rocket and handle its trajectory;

--- src/144RGBcode/firework.ino
+++ src/144RGBcode/firework.ino
@@ -1,6 +1,38 @@
+typedef struct color {
+  int r;
+  int g;
+  int b;
+} color;
+
+typedef struct rocket {
+  int x;
+  int y;
+  color c;
+  int timer;
+  int maxHeight;
+  int layer;
+} rocket;
+
 void firework(int seconds) {
-  int fireworkArray[6][12];
-  for(int i = 0; i < seconds; i++) {
-    
-  }
+  rocket rocketArray[10];
+}
+
+struct rocket spawnRocket(int l) {
+  rocket r; 
+  r.x = random(16);
+  r.y = 0;
+  color c;
+  c.r = random(2);
+  c.g = random(2);
+  c.b = random(2);
+  r.c = c;
+  r.timer = random(30);
+  r.maxHeight = random(6);
+  r.layer = l;
+  return r;
+}
+
+void explodeRocket(rocket r) {
+  color c = r.c;
+  setLedOn(r.x,r.y,c.r,c.g,c.b,r.layer);
 }
