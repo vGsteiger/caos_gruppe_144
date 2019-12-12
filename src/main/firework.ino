a8287ce1c58319659510c63d9d090614d71aac1e
Viktor Urban <v.gsteiger@gmail.com>
2019-12-12 10:02:11 +0100

    Implemented a first version of Firework
    Worked with structs to implement a first version of firework which will spawn random rockets at random places and then make them run.

--- src/main/firework.ino
+++ src/main/firework.ino
@@ -5,10 +5,24 @@ typedef struct rocket {
   int timer;
   int maxHeight;
   int layer;
+  boolean burnt;
 } rocket;
 
+rocket rocketArray0[6];
+rocket rocketArray1[6];
+
 void firework(int seconds) {
-  rocket rocketArray[10];
+  for(int i = 0; i < 6; i++) {
+    rocketArray0[i] = spawnRocket(0);
+    rocketArray0[i] = spawnRocket(1);
+  }
+  for(int t = 0; t < seconds; t++) {
+    for(int i = 0; i < 6; i++) {
+      burnRocket(rocketArray0[i], i);
+      burnRocket(rocketArray1[i], i);
+      shiftToShifter(1000);
+    }
+  }
 }
 
 struct rocket spawnRocket(int l) {
@@ -20,13 +34,44 @@ struct rocket spawnRocket(int l) {
   c.g = random(2);
   c.b = random(2);
   r.c = c;
-  r.timer = random(30);
+  r.timer = 0;
   r.maxHeight = random(6);
   r.layer = l;
   return r;
 }
 
-void explodeRocket(rocket r) {
-  color c = r.c;
-  setLedOn(r.x,r.y,c.r,c.g,c.b,r.layer);
+struct rocket explodeRocket(rocket r) {
+   color c = r.c;
+   setLedOn(r.x,r.y,c.r,c.g,c.b,r.layer);
+   setLedOn(r.x-1,r.y-1,c.r,c.g,c.b,r.layer);
+   setLedOn(r.x+1,r.y+1,c.r,c.g,c.b,r.layer);
+   setLedOn(r.x+1,r.y-1,c.r,c.g,c.b,r.layer);
+   setLedOn(r.x-1,r.y+1,c.r,c.g,c.b,r.layer);
+   return spawnRocket(r.layer);
+}
+
+void burnRocket(rocket r, int iterate) {
+  if(r.timer == r.maxHeight) {
+    if(r.layer == 0) {
+      rocketArray0[iterate] = explodeRocket(r);
+      return;
+    } else {
+      rocketArray1[iterate] = explodeRocket(r);
+      return;
+    }
+  } else{
+    color c = r.c;
+    setLedOn(r.x,r.y,c.r,c.g,c.b,r.layer);
+    r.y++;
+    int ran = random(100);
+    if(r.x > 0 && r.x < 15) {
+      if(ran < 15) {
+        r.x--;
+      } else if(ran < 30) {
+        r.x++;
+      }
+    }
+    r.timer++;
+    return;
+  }
 }
