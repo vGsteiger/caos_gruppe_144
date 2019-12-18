2d8987ef1fb0a1e67ed37ef4d866529c0b412d0c
Viktor Urban <v.gsteiger@gmail.com>
2019-12-18 19:40:34 +0100

    Changed the multiplexing algorithm
    Now only one Bitshifter is shifted out every hertz thus reducing the strain on the overall system.

--- src/main/bitShifterLogic.ino
+++ src/main/bitShifterLogic.ino
@@ -1,33 +1,35 @@
 void shiftToShifter(int miliSeconds) 
   {
-    int hertz = 120;
-    int delayProHertz = (int)(miliSeconds/hertz)/2;
+    int hertz = 142;
+    int delayProHertz = (int)(miliSeconds/hertz);
     for(int s = 0; s < miliSeconds; s++) {
-    for(int hrtz = 0; hrtz < hertz; hrtz++) {
-    changeLayer(0);
-    digitalWrite(blank_pin, HIGH);//shut down the leds
-    for(int i = currentAmountOfShifters-1; i >= 0; i--) {
-      SPI.transfer(anodes0[i]);
-    }
-   
-    // shift register to storage register
-    digitalWrite(latch_pin, HIGH);
-    digitalWrite(latch_pin, LOW);
-    digitalWrite(blank_pin, LOW);  //enable pins
-    delay(delayProHertz);
-    
-    changeLayer(1);
-    digitalWrite(blank_pin, HIGH);//shut down the leds
-    for(int i = currentAmountOfShifters-1; i >= 0; i--) {
-      SPI.transfer(anodes1[i]);
-    }
-   
-    // shift register to storage register
-    digitalWrite(latch_pin, HIGH);
-    digitalWrite(latch_pin, LOW);
-    digitalWrite(blank_pin, LOW);  //enable pins
-    delay(delayProHertz);
-    }
+      int currentShifter = currentAmountOfShifters-1;
+      for(int hrtz = 0; hrtz < hertz; hrtz++) {
+        changeLayer(0);
+        digitalWrite(blank_pin, HIGH);//shut down the leds
+        for(int i = currentShifter; i >= currentShifter-1; i--) {
+          SPI.transfer(anodes0[i]);
+        }
+       
+        // shift register to storage register
+        digitalWrite(latch_pin, HIGH);
+        digitalWrite(latch_pin, LOW);
+        digitalWrite(blank_pin, LOW);  //enable pins
+        delay(delayProHertz);
+        
+        changeLayer(1);
+        digitalWrite(blank_pin, HIGH);//shut down the leds
+        for(int i = currentShifter; i >= currentShifter-1; i--) {
+          SPI.transfer(anodes1[i]);
+        }
+       
+        // shift register to storage register
+        digitalWrite(latch_pin, HIGH);
+        digitalWrite(latch_pin, LOW);
+        digitalWrite(blank_pin, LOW);  //enable pins
+        delay(delayProHertz);
+        currentShifter = currentShifter - 1;
+      }
     }
     memset(anodes0, 0, sizeof(anodes0));
     memset(anodes1, 0, sizeof(anodes1));
