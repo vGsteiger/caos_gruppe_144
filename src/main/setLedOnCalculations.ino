b3110431e32d3e125a82fd74ad12895a44d490ee
Viktor Urban <v.gsteiger@gmail.com>
2020-01-09 19:13:39 +0100

    Fixed the error with out setLedOn

--- src/main/setLedOnCalculations.ino
+++ src/main/setLedOnCalculations.ino
@@ -1,3 +1,4 @@
+
 void setLedOn(int x, int y, int red, int green, int blue, int layer)
 {
   x = constrain (x,    0, 11);      // x can only be between 0 and 11 as we have 12 leds length
@@ -10,7 +11,8 @@ void setLedOn(int x, int y, int red, int green, int blue, int layer)
   int whichByte = int(((x * 3 + 36 * y) + 1) / 8); // Calculate which byte be have to change
   int whichBit = ((((y * 36 + x * 3) + 1) % 8) - 1); //  Calculate which bit in that byte we have to set on
   if(whichBit == -1) {
-    whichBit = 3;
+    whichBit = 7;
+    whichByte--;
   }
   Serial.println("Currently in byte:");
   Serial.println(whichByte);
@@ -20,7 +22,7 @@ void setLedOn(int x, int y, int red, int green, int blue, int layer)
   /**
      Choses between the two available layers of our LED RGB Cubic.
   */
-      if (whichBit == 7) {
+      if (whichBit == 7) { // The mistake with the colors must be here!
         bitWrite(anodes0[whichByte], whichBit, blue);
         bitWrite(anodes0[whichByte + 1], 0, green);
         bitWrite(anodes0[whichByte + 1], 1, red);
@@ -33,6 +35,4 @@ void setLedOn(int x, int y, int red, int green, int blue, int layer)
         bitWrite(anodes0[whichByte], whichBit + 1, green);
         bitWrite(anodes0[whichByte], whichBit + 2, red);
       }
-  //Serial.println(anodes0[2]);
-
 }
